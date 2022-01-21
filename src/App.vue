875a753b80199d1dfba2c7afddc920caa1271c41
오창영 <yeongfactory@gmail.com>
2022-01-21 12:17:34 +0900

    first init

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div> -->
+  <router-view />
+</template>
+
+<style>
+html,
+body,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+input,
+select,
+ul,
+ol {
+  margin: 0;
+  padding: 0;
+}
+ul,
+ol,
+dl {
+  list-style: none;
+}
+html,
+body,
+#app {
+  height: 100%;
+}
+</style>
---
+++ src/views/Edit.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="edit">
+    <div class="top"></div>
+    <div class="bottom">
+      <div class="hierarchy"></div>
+      <div class="canvas"></div>
+      <!-- <div class="inspector"></div> -->
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.edit {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+.top {
+  height: 40px;
+  background: #ccc;
+  border-bottom: 1px solid #000;
+}
+.bottom {
+  display: flex;
+  flex: 1;
+}
+.hierarchy {
+  width: 100px;
+  background: #ccc;
+}
+</style>
